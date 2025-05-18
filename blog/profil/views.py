85718fbfec735f98cf543de0cff49a24fa012f49
mnovaldr <noval.compani@gmail.com>
2025-05-18 20:51:34 +0700

    Update Bootstrap dan dark mode

--- blog/profil/views.py
+++ blog/profil/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.template import RequestContext
+
+
+def view404(request, *args, **argv):
+    response = render('404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
 
 # Create your views here.
 def home(request):
