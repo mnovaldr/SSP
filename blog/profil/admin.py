b8b0f753aa71a784198793af70074531bbae8179
mnovaldr <mnovaldr@gmail.com>
2025-06-02 17:24:13 +0700

    Update Models

--- blog/profil/admin.py
+++ blog/profil/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Barang
 
 # Register your models here.
+
+class BarangAdmin(admin.ModelAdmin):
+    list_display = ('kdbrg', 'nama', 'stok', 'harga', 'link_gbr')
+    search_fields = ('nama',)
+
+admin.site.register(Barang, BarangAdmin)
--- blog/profil/models.py
+++ blog/profil/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Barang(models.Model):
+    kdbrg = models.CharField(max_length=8)
+    nama = models.CharField(max_length=75)
+    stok = models.IntegerField()
+    harga = models.BigIntegerField()
+    link_gbr = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return self.nama
